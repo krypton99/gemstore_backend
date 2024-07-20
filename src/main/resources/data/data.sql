44ff8123606a5cc10d5173b2077854fdff0dda5e
krypton99 <17520777@gm.uit.edu.vn>
2024-07-20 19:20:28 +0700

    Change dependencies and add data.sql for data initialization, change some properties in application.properties

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,49 @@
+insert into attribute_type(name, is_deleted) values 
+			("sign", false),
+            ("element", false),
+            ("planet", false),
+            ("chakra", false);
+            
+            
+insert into attribute(name, icon_url, is_delete, attribute_type_id) values 
+			("scorpio", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/scorpio.svg", false, 1),
+			("pisces", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/pisces.svg", false, 1),
+            ("aquarius", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/aquarius.svg", false, 1),
+            ("gemini", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/gemini.svg", false, 1),
+            ("libra", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/libra.svg", false, 1),
+            ("virgo", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/virgo.svg", false, 1),
+            ("leo", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/leo.svg", false, 1),
+            ("aries", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/aries.svg", false, 1),
+            ("cancer", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/cancer.svg", false, 1),
+            ("capricorn", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/capricorn.svg", false, 1),
+            ("sagittarius", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/sagittarius.svg", false, 1),
+            ("taurus", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/zodiac-signs/taurus.svg", false, 1);
+            
+insert into attribute(name, icon_url, is_delete, attribute_type_id) values 
+			("air", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/elements/wind.svg", false, 2),
+			("earth", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/elements/earth.svg", false, 2),
+            ("fire", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/elements/fire.svg", false, 2),
+            ("water", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/elements/water.svg", false, 2);
+            
+insert into attribute(name, icon_url, is_delete, attribute_type_id) values 
+			("venus", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/venus.svg", false, 3),
+			("mercury", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/mercury.svg", false, 3),
+			("sun", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/sun.svg", false, 3),
+			("moon", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/moon.svg", false, 3),
+			("saturn", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/saturn.svg", false, 3),
+            ("jupiter", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/jupiter.svg", false, 3),
+            ("mars", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/mars.svg", false, 3),
+            ("neptune", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/neptune.svg", false, 3),
+			("uranus", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/uranus.svg", false, 3),
+			("pluto", "https://www.moonomens.com/wp-content/themes/moonomens/images/crystal/planets/pluto.svg", false, 3);
+
+insert into attribute(name, is_delete, attribute_type_id) values 
+			("root", false, 4),
+            ("heart", false, 4),
+            ("third eye", false, 4),
+            ("throat", false, 4),
+            ("solar plexus", false, 4),
+            ("sacral", false, 4),
+            ("crown", false, 4)
+
+
